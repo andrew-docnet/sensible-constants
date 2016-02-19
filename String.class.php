04e7bf5e6a36d22a67e1eba1264fc9657f88a565
andrew baillie <abaillie@docnet.nu>
2016-02-19 12:40:12 +0000

    Some useful strings

---
+++ String.class.php
@@ -0,0 +1,36 @@
+<?php
+interface String {
+   const
+      STR_DUCK = 'Duck',
+      STR_L_DUCK = 'duck',
+      STR_U_DUCK = 'DUCK',
+
+      STR_HORSE = 'Horse',
+      STR_L_HORSE = 'horse',
+      STR_U_HORSE = 'HORSE',
+
+      STR_COW = 'Cow',
+      STR_L_COW = 'cow',
+      STR_U_COW = 'COW',
+
+      STR_CHAIR = 'Chair',
+      STR_L_CHAIR = 'chair',
+      STR_U_CHAIR = 'CHAIR',
+
+      STR_GLASS = 'Glass',
+      STR_L_GLASS = 'glass',
+      STR_U_GLASS = 'GLASS',
+
+      STR_PHONE = 'Phone',
+      STR_L_PHONE = 'phone',
+      STR_U_PHONE = 'PHONE',
+
+      STR_DEVELOPER = 'Developer',
+      STR_L_DEVELOPER = 'developer',
+      STR_U_DEVELOPER = 'DEVELOPER',
+
+      STR_FLARGLEPARP = 'Flargleparp',
+      STR_L_FLARGLEPARP = 'flargleparp',
+      STR_U_FLARGLEPARP = 'FLARGLEPARP',
+   ;
+}
