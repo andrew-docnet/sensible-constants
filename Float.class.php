4ce34aaa4047f62d69f413e35bb5a4b5a64050ce
Red Sub Dev <rssupport@docnet.nu>
2016-04-18 12:17:25 +0100

    starting off on IEEE754

---
+++ Float.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Float.class.php
+ *
+ * Sensible constants for IEEE754 Floats
+ */
+namespace Docnet\SensibleConstants;
+
+interface Float {
+   const
+      // pi milk?
+      MILK = 3.1415926535897932384626433832795
+   ;
+}
